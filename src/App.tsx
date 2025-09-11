bc31608b33e2480e23994c36458226ac00592970
paulphilip96 <paulphilip290996@gmail.com>
2025-09-10 20:37:52 -0500

    added app version in footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import Homepage from "./Pages/Homepage/Homepage";
 import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 
-import './App.scss'
+import "antd/dist/reset.css";
+import "./App.scss"
 
 
 function App() {
--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,19 +1,28 @@
+import { Tag } from "antd";
 import moment from "moment";
+
+import packageJson from "../../../package.json";
 
 import "./Footer.scss";
 
 
 const Footer = () => {
-  
+
   const getCurrentYear = (): number => {
     const now = moment();
     return now.year();
   }
 
+  const appVersion = packageJson.version;
+
   return (
     <div className="Footer">
-      <hr />
-      <div>&copy; {getCurrentYear()} Paul Philip. Built / hosted with React, Node.js & AWS</div>
+      <Tag color="gold">{`v${appVersion}`}</Tag>
+
+      <div className="Footer__Right">
+        <hr />
+        <div>&copy; {getCurrentYear()} Paul Philip. Built / hosted with React, Node.js & AWS</div>
+      </div>
     </div>
   )
 }
